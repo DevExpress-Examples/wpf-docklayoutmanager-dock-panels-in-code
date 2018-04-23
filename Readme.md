1a847628d2b8b365cb3ee5fa18658f1241c80523
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to: Dock a Panel to Another Panel in Code
+
+
+<p>This example shows how to dock a panel in code using the methods provided by the <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfDockingDockLayoutManager_DockControllertopic">DockLayoutManager.DockController</a> object.</p>
+<p>In the Button1_Click event handler, a panel is docked to another panel forming a split container (two panels are displayed side by side). In the Button2_Click event handler, a panel is docked to another panel forming a tabbed group.</p>
+
+<br/>
+
+
---
+++ VB/DockPanelInCode_Ex/Window1.xaml.vb
@@ -0,0 +1,39 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Layout.Core
+
+Namespace DockPanelInCode_Ex
+	''' <summary>
+	''' Interaction logic for Window1.xaml
+	''' </summary>
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		#Region "#1"
+		Private Sub Button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			' Dock the Properties panel at the bottom of the Output panel.
+			dockManager1.DockController.Dock(paneProperties, paneOutput, DockType.Bottom)
+		End Sub
+
+		Private Sub Button2_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			' Dock the Properties panel to the Output panel, creating a tab container.
+			dockManager1.DockController.Dock(paneProperties, paneOutput, DockType.Fill)
+		End Sub
+		#End Region ' #1
+	End Class
+End Namespace
